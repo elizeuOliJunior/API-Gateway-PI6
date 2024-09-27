25437836ab523540fd5f0c31f0bc4c2bddc8b408
elizgoj <juniorelizeu53@gmail.com>
2024-09-27 17:01:08 -0300

    configuracoes iniciais gtw

---
+++ src/main/java/br/com/elizeu/springcloudgtw/SpringCloudGtwApplication.java
@@ -0,0 +1,13 @@
+package br.com.elizeu.springcloudgtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGtwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elizeu/springcloudgtw/SpringCloudGtwApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elizeu.springcloudgtw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
